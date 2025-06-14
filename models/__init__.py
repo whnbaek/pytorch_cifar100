3c468b9fde799e9cca7166b0d102557738164f66
Woohyeon Baek <whnbaek@gmail.com>
2025-06-14 14:38:59 -0400

    Wrap pytorch_fiar100.models as a package

---
+++ models/__init__.py
@@ -0,0 +1,32 @@
+from .attention import attention56, attention92
+from .densenet import densenet121, densenet161, densenet169, densenet201
+from .googlenet import googlenet
+from .inceptionv3 import inceptionv3
+from .inceptionv4 import inceptionv4, inception_resnet_v2
+from .mobilenet import mobilenet
+from .mobilenetv2 import mobilenetv2
+from .nasnet import nasnet
+from .preactresnet import (
+    preactresnet18,
+    preactresnet34,
+    preactresnet50,
+    preactresnet101,
+    preactresnet152,
+)
+from .resnet import resnet18, resnet34, resnet50, resnet101, resnet152
+from .resnext import resnext50, resnext101, resnext152
+from .rir import resnet_in_resnet
+from .senet import seresnet18, seresnet34, seresnet50, seresnet101, seresnet152
+from .shufflenet import shufflenet
+from .shufflenetv2 import shufflenetv2
+from .squeezenet import squeezenet
+from .stochasticdepth import (
+    stochastic_depth_resnet18,
+    stochastic_depth_resnet34,
+    stochastic_depth_resnet50,
+    stochastic_depth_resnet101,
+    stochastic_depth_resnet152,
+)
+from .vgg import vgg11_bn, vgg13_bn, vgg16_bn, vgg19_bn
+from .wideresidual import wideresnet
+from .xception import xception
